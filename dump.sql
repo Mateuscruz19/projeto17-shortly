230541095e4395d53ca0a537834208e3414b2215
Mateuscruz19 <profissionalmateuscruz@gmail.com>
2023-02-26 22:38:08 -0300

    Dump sql fixed

--- dump.sql
+++ dump.sql
@@ -60,9 +60,9 @@ ALTER SEQUENCE public.sessions_id_seq OWNED BY public.sessions.id;
 CREATE TABLE public.shortens (
     id integer NOT NULL,
     url text NOT NULL,
-    shorturl text NOT NULL,
-    userid integer NOT NULL,
-    createdat timestamp without time zone DEFAULT now() NOT NULL,
+    "shortUrl" text NOT NULL,
+    "userId" integer NOT NULL,
+    "createdAt" timestamp without time zone DEFAULT now() NOT NULL,
     views integer DEFAULT 0 NOT NULL
 );
 
@@ -145,6 +145,16 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 -- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: -
 --
 
+INSERT INTO public.sessions VALUES (1, '36b6e578-ca0f-4c4c-bf10-0b384b3b3297', true, 1, '2023-02-26 00:30:01.72396');
+INSERT INTO public.sessions VALUES (2, '1f08aae7-29b1-4349-8a51-18071d1d44ed', true, 1, '2023-02-26 00:32:43.877994');
+INSERT INTO public.sessions VALUES (3, '66649901-1e9b-4ffb-822c-723115e7d24f', true, 1, '2023-02-26 00:49:44.274245');
+INSERT INTO public.sessions VALUES (4, 'f55ee8b3-0403-474b-b74b-b07332b32c27', true, 1, '2023-02-26 18:43:20.119953');
+INSERT INTO public.sessions VALUES (5, '2811612e-cf52-47a4-8e7b-e1f67a452e27', true, 1, '2023-02-26 21:10:52.314325');
+INSERT INTO public.sessions VALUES (6, '04af04f6-385b-4b2c-9277-dda2ad54a3ca', true, 1, '2023-02-26 22:04:28.168259');
+INSERT INTO public.sessions VALUES (7, '9fedaf67-e6a6-442f-b646-921bc7987845', true, 1, '2023-02-26 22:08:00.860031');
+INSERT INTO public.sessions VALUES (8, 'd1a9db04-108f-48aa-ac88-a9319995bb71', true, 1, '2023-02-26 22:09:57.533502');
+INSERT INTO public.sessions VALUES (9, '5d65b22c-bd21-4c00-b7a6-c346112b18c2', true, 1, '2023-02-26 22:12:04.44234');
+INSERT INTO public.sessions VALUES (10, '8cca6346-c078-46bd-a8f2-92fda5ec05ac', true, 1, '2023-02-26 22:12:34.907589');
 
 
 --
@@ -164,7 +174,7 @@ INSERT INTO public.users VALUES (1, 'joao@driven.com.br', '$2b$10$6NaQel3Qsf2q2w
 -- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.sessions_id_seq', 1, false);
+SELECT pg_catalog.setval('public.sessions_id_seq', 10, true);
 
 
 --
@@ -214,11 +224,11 @@ ALTER TABLE ONLY public.sessions
 
 
 --
--- Name: shortens shortens_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+-- Name: shortens shortens_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.shortens
-    ADD CONSTRAINT shortens_userid_fkey FOREIGN KEY (userid) REFERENCES public.users(id);
+    ADD CONSTRAINT "shortens_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id);
 
 
 --
