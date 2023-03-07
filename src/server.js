98bb5afbeafd4cdb8eb5ed5e5765e8921859d856
Mateuscruz19 <profissionalmateuscruz@gmail.com>
2023-03-07 14:18:30 -0300

    Deply

--- src/server.js
+++ src/server.js
@@ -14,5 +14,5 @@ app.use(Auth);
 app.use(Urls)
 app.use(User)
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running in port: ${port}`));
